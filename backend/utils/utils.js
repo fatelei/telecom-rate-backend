e6298395b4c3f57b78880a3d50fcb9ab53170af2
fatelei <fatelei@gmail.com>
2016-04-29 18:34:04 +0800

    Fixup

--- backend/utils/utils.js
+++ backend/utils/utils.js
@@ -3,7 +3,7 @@
 const querystring = require('querystring')
 const url = require('url')
 
-exports.generatePaging = (host, path, params) {
+exports.generatePaging = (host, path, params) => {
   return url.format({
     protocol: 'http',
     host: host,
