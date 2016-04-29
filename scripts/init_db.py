7acd3bc8dec88786d55937a09049140b191ba37d
fatelei <fatelei@gmail.com>
2016-04-29 17:36:35 +0800

    Fixup

--- scripts/init_db.py
+++ scripts/init_db.py
@@ -5,7 +5,7 @@ import pymysql.cursors
 
 # Connect to the database
 connection = pymysql.connect(host='localhost',
-                             user='user',
+                             user='root',
                              password='123456',
                              db='telecom',
                              charset='utf8mb4',
@@ -43,8 +43,15 @@ try:
         # Create a new record
         for i in xrange(100):
             updated_at, created_at = int(time.time()), int(time.time())
-            sql = "INSERT INTO `users` (`email`, `password`) VALUES (%s, %s)"
-            cursor.execute(sql, ('webmaster@python.org', 'very-secret'))
+            sql = "INSERT INTO `product` (`name`, `description`, `image_url`, `type`, `created_at`, `updated_at`) VALUES (%s, %s, %s, %s, %s, %s)"
+            cursor.execute(sql, (
+                products[i]["name"],
+                products[i]["description"],
+                products[i]["image_url"],
+                products[i]["type"],
+                products[i]["created_at"],
+                products[i]["updated_at"]
+            ))
 
     # connection is not autocommit by default. So you must commit to save
     # your changes.
